f27a87aa3ace55761f2642b33d9878e0e9695f63
Andika MF <irukainu20@gmail.com>
2024-04-19 22:10:38 +0700

    3

---
+++ controller/coba.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/aiteung/musik"
+	"github.com/gofiber/fiber/v2"
+	cek "github.com/indrariksa/cobapakcage/module"
+	"github.com/nekowawolf/ws-andikamf/config"
+)
+
+func Homepage(c *fiber.Ctx) error {
+	ipaddr := musik.GetIPaddress()
+	return c.JSON(ipaddr)
+}
+
+func GetPresensi(c *fiber.Ctx) error {
+	ps := cek.GetAllPresensi(config.Ulbimongoconn, "presensi")
+	return c.JSON(ps)
+}
