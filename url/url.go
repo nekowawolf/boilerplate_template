669b3de6c78750b28e441d9de61bde4ee86d9140
nekowawolf <irukainu20@gmail.com>
2024-06-08 00:26:09 +0700

    week11

--- url/url.go
+++ url/url.go
@@ -20,4 +20,6 @@ func Web(page *fiber.App) {
 	page.Get("/presensi", controller.GetPresensi)
 	page.Get("/presensi/:id", controller.GetPresensiID) //menampilkan data presensi berdasarkan id
 	page.Post("/insert", controller.InsertDataPresensi)
+	page.Put("/update/:id", controller.UpdateData)
+	page.Delete("/delete/:id", controller.DeletePresensiByID)
 }
