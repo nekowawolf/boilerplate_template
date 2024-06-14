681228ea8f4f91a25fe0f696383e54e6016401c0
nekowawolf <irukainu20@gmail.com>
2024-06-14 15:19:52 +0700

    add : swagger2

--- main.go
+++ main.go
@@ -11,7 +11,20 @@ import (
 	"github.com/nekowawolf/ws-andikamf/url"
 
 	"github.com/gofiber/fiber/v2"
+	_ "github.com/nekowawolf/ws-andikamf/docs"
 )
+
+// @title TES SWAGGER ULBI
+// @version 1.0
+// @description This is a sample swagger for Fiber
+
+// @contact.name API Support
+// @contact.url https://github.com/nekowawolf
+// @contact.email andikamusk@gmail.com
+
+// @host  ws-andikamf-8a9eea7e7e50.herokuapp.com
+// @BasePath /
+// @schemes https http
 
 func main() {
 	site := fiber.New(config.Iteung)
